47c2a86f910254231b2d5c3cdfc14e2918a44b06
marcoHoev <marco.hoev@gmail.com>
2022-05-30 12:34:36 +0100

    Added script for installing packages using Rscript in terminal

---
+++ install_packages.r
@@ -0,0 +1,4 @@
+install.packages("R2jags",dependencies=TRUE,repos="http://cran.us.r-project.org")
+install.packages("runjags",dependencies=TRUE,repos="http://cran.us.r-project.org")
+install.packages("MCMCpack",dependencies=TRUE,repos="http://cran.us.r-project.org")
+install.packages("fitdistrplus", dependencies=TRUE,repos="http://cran.us.r-project.org")
