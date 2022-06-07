37900dab0af2d47580767a5a49300409d8b31c87
marcelbra <marcelbraasch@gmail.com>
2022-06-07 14:57:45 +0100

    Model /w 2 cp, compiles but wrong

--- main.r
+++ main.r
@@ -36,22 +36,12 @@ for (j in 1:M) {
   SPI_counted$x <- cumulative
   names(SPI_counted)[ncol(SPI_counted)] <- names[j]  # Rename
 }
-
-# Parameters to be estimated
-plp.mod.params <- c("alpha1", "sigma1")
-
-# Provide the data (for `1 mês` series)
-
 y <- SPI_counted$"1 mês"
-plp.mod.data <- list("y", "N")
 
-# Define model
-plp.mod <- function() {
-  # Likelihood
-  for (i in 1:N) {
-    y[i] ~ dpois((i/sigma1)**(alpha1))
-  }
-  # Prior
+plp.mod.params <- c("alpha1", "sigma1") # Params to be estimated
+plp.mod.data <- list("y", "N") # Data to be fed
+plp.mod <- function() { # Define model
+  for (i in 1:N) { y[i] ~ dpois((i/sigma1)**(alpha1)) } 
   alpha1 ~ dunif(1e-5, 100)
   sigma1 ~ dunif(1e-5, 100)
 }
@@ -66,24 +56,84 @@ print(plp.mod.fit)
 
 # Plot process by thinning procedure
 # https://stats.stackexchange.com/questions/369288/nonhomogeneous-poisson-process-simulation
-sigma_hat <- plp.mod.fit$BUGSoutput[11]$mean$sigma1
-alpha_hat <- plp.mod.fit$BUGSoutput[11]$mean$alpha1
-rate <- function(t) {(t/sigma_hat)**(alpha_hat)}
-intensity <- function(t) {(alpha_hat/sigma_hat)*(t/sigma_hat)**(alpha_hat-1)}
-p <- function(t) {intensity(t)/alpha_hat}
-S <- c()
-t <- 0
-I <- 0
-for (t in 1:N) {
-  U_1 <- runif(1)
-  t <- t - (log(U_1)/alpha_hat)
-  U_2 <- runif(1)
-  if (U_2 < p(t)) {I <- I + 1}
-  S <- c(S, I)
-}
+sim <- function(){
+  sigma_hat <- plp.mod.fit$BUGSoutput[11]$mean$sigma1
+  alpha_hat <- plp.mod.fit$BUGSoutput[11]$mean$alpha1
+  rate <- function(t) {(t/sigma_hat)**(alpha_hat)}
+  intensity <- function(t) {(alpha_hat/sigma_hat)*(t/sigma_hat)**(alpha_hat-1)}
+  p <- function(t) {intensity(t)/alpha_hat}
+  S <- c()
+  t <- 0
+  I <- 0
+  for (t in 1:N) {
+    U_1 <- runif(1)
+    t <- t - (log(U_1)/alpha_hat)
+    U_2 <- runif(1)
+    if (U_2 < p(t)) {I <- I + 1}
+    S <- c(S, I)
+  }
+  S
+} 
 
 # Plot the original data + process
-plot(0,0,xlim = c(0,N),ylim = c(0,max(y)), type = "n", main = "")
-lines(stepfun(1:(length(S)-1), S), cex.points = 0.1, lwd=0, col = "#FF0000")
-lines(stepfun(1:(length(y)-1), y), cex.points = 0.1, lwd=0, col = "#000000")
+plot_data_and_process <- function(y, S) {
+  plot(0,0,xlim = c(0,N),ylim = c(0,max(y)), type = "n", main = "")
+  lines(stepfun(1:(length(S)-1), S), cex.points = 0.1, lwd=0, col = "#FF0000")
+  lines(stepfun(1:(length(y)-1), y), cex.points = 0.1, lwd=0, col = "#000000")
+}
 
+
+###############################################################################
+############################# 2 Changepoints ##################################
+###############################################################################
+
+# Model for two changepoints
+plp.mod <- function() {
+  for (i in 1:N) {
+    y[i] ~ dpois(ifelse(i<=tau1,
+                        (i/sigma1)**alpha1,
+                        (tau1/sigma1)**alpha1 + (i/sigma2)**alpha2 - (tau1/sigma2)**alpha2
+                        )
+                 )
+  }
+  alpha1 ~ dunif(1e-5, 100)
+  sigma1 ~ dunif(1e-5, 100)
+  alpha2 ~ dunif(1e-5, 100)
+  sigma2 ~ dunif(1e-5, 100)
+  tau1 ~ dunif(0,N)
+}
+
+plp.mod.params <- c("alpha1", "sigma1", "alpha2", "sigma2", "tau1") # Params to be estimated
+plp.mod.data <- list("y", "N")#, "sigma", "alpha") # Data to be fed
+
+# Fit model
+plp.mod.fit <- jags(data = plp.mod.data, 
+                    parameters.to.save = plp.mod.params,
+                    #n.chains = 1, n.iter = 200,
+                    #n.burnin = 100, model.file = plp.mod)
+                    n.chains = 3, n.iter = 12000,
+                    n.burnin = 10000, model.file = plp.mod)
+plot(plp.mod.fit)
+print(plp.mod.fit)
+
+#simulate <- function(sigmas, alphas, taus) {
+#  #sigma_hat <- plp.mod.fit$BUGSoutput[11]$mean$sigma1
+#  #alpha_hat <- plp.mod.fit$BUGSoutput[11]$mean$alpha1
+#  sigma_hat <- sigmas
+#  alpha_hat <- alphas
+#  tau_hat <- taus
+#  rate <- function(t) {(t/sigma_hat)**(alpha_hat)}
+#  intensity <- function(t) {(alpha_hat/sigma_hat)*(t/sigma_hat)**(alpha_hat-1)}
+#  p <- function(t) {intensity(t)/alpha_hat}
+#  S <- c()
+#  t <- 0
+#  I <- 0
+#  for (t in 1:N) {
+#    U_1 <- runif(1)
+#    t <- t - (log(U_1)/alpha_hat)
+#    U_2 <- runif(1)
+#    if (U_2 < p(t)) {I <- I + 1}
+#    S <- c(S, I)
+#  }
+#  S
+#}
